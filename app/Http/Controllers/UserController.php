cffa4a89ea670aaa4bc58bff12ba98d936f6c153
Ariel Saputra <m15759246@gmail.com>
2024-03-12 07:26:42 +0700

    Jobsheet4 - Praktikum 2.7-Relationship

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,10 +16,9 @@ class UserController extends Controller
     //     ]);
     // }
 
-    public function index()
-    {
-        // Tambah data user dengan Eloquement Model
-        /*$data = [
+
+    // Tambah data user dengan Eloquement Model
+    /*$data = [
             'level_id' => 3,
             'user_id' => 4,
             'username' => 'customer-1',
@@ -28,74 +27,77 @@ class UserController extends Controller
         ];
         UserModel::insert($data);*/
 
-        // Modifikasi
-        // $data = [
-        //     'nama' => 'Pelanggaran Pertama'
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); // Update data user
+    // Modifikasi
+    // $data = [
+    //     'nama' => 'Pelanggaran Pertama'
+    // ];
+    // UserModel::where('username', 'customer-1')->update($data); // Update data user
 
-        // Modifikasi Joobshet Pertemuan 4 - Praktikunm 1
-        // $data = [
-        //     'user_id' => 5, // agar user_id pada table terisi semua
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
+    // Modifikasi Joobshet Pertemuan 4 - Praktikunm 1
+    // $data = [
+    //     'user_id' => 5, // agar user_id pada table terisi semua
+    //     'level_id' => 2,
+    //     'username' => 'manager_tiga',
+    //     'nama' => 'Manager 3',
+    //     'password' => Hash::make('12345'),
+    // ];
+    // UserModel::create($data);
 
-        // Coba akses model UserModel
-        /*$user = UserModel::findOr(20, ['username', 'nama'], function () {
+    // Coba akses model UserModel
+    /*$user = UserModel::findOr(20, ['username', 'nama'], function () {
             abort(404);
         }); // Ambil semua data dari tabel m_user
         return view('level/index', ['data' => $user]);*/
 
-        // Praktikum 2.2
-        // $user = UserModel::where('username', 'manager9')->findOrFail();
-        // return view('level/index', ['data' => $user]);
-        // Praktikum 2.3
-        // $user = UserModel::where('level_id', 2)->count();
-        // return view('level/index', ['data' => $user]);
+    // Praktikum 2.2
+    // $user = UserModel::where('username', 'manager9')->findOrFail();
+    // return view('level/index', ['data' => $user]);
+    // Praktikum 2.3
+    // $user = UserModel::where('level_id', 2)->count();
+    // return view('level/index', ['data' => $user]);
 
-        // $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager33',
-        //         'nama' => 'Manager Tiga Tiga',
-        //     ],
-        // );
-        //$user->save();
+    // $user = UserModel::firstOrNew(
+    //     [
+    //         'username' => 'manager33',
+    //         'nama' => 'Manager Tiga Tiga',
+    //     ],
+    // );
+    //$user->save();
 
 
-        // Praktikum 2.5
-        $user = UserModel::all();
-        //     [
-        //         'username' => 'manager11',
-        //         'nama' => 'Manager11',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ]
-        // );
+    // Praktikum 2.5
 
-        // $user->username = 'manager12';
+    //     [
+    //         'username' => 'manager11',
+    //         'nama' => 'Manager11',
+    //         'password' => Hash::make('12345'),
+    //         'level_id' => 2
+    //     ]
+    // );
 
-        // $user->save(); //save
+    // $user->username = 'manager12';
 
-        // $user->wasChanged(); //true
-        // $user->wasChanged('username'); //true
-        // $user->wasChanged(['username', 'level_id']); //false
-        // $user->wasChanged('nama'); //false
-        // //$user->wasChanged(['nama', 'username']); //true
+    // $user->save(); //save
 
-        // dd($user->wasChanged(['nama', 'username'])); //true
+    // $user->wasChanged(); //true
+    // $user->wasChanged('username'); //true
+    // $user->wasChanged(['username', 'level_id']); //false
+    // $user->wasChanged('nama'); //false
+    // //$user->wasChanged(['nama', 'username']); //true
 
-        // $user->isClean(); //false
-        // $user->isClean('username'); //false
-        // $user->isClean('nama'); //true
-        // $user->isClean(['nama', 'username']); //false
+    // dd($user->wasChanged(['nama', 'username'])); //true
 
-        // $user->isDirty(); //false
-        // $user->isClean(); //true
+    // $user->isClean(); //false
+    // $user->isClean('username'); //false
+    // $user->isClean('nama'); //true
+    // $user->isClean(['nama', 'username']); //false
 
+    // $user->isDirty(); //false
+    // $user->isClean(); //true
+
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
         return view('level/index', ['data' => $user]);
     }
     public function tambah()
