8ae1dce987ee0fe99615b103be6df2d649b4e616
Ariel Saputra <m15759246@gmail.com>
2024-03-18 12:22:29 +0700

    Jobsheet5 - Praktikum3-Membuat form, dan menyimpan ke database

--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,8 @@ Route::get('/level', [LevelController::class, 'index']);
 
 // Routing Kategori page
 Route::get('/kategori', [KategoriController::class, 'index']); //a->name('manage.category')
-// Route::get('/kategori/create', [KategoriController::class, 'create'])->name('category.create');
-// Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/create', [KategoriController::class, 'create']); //->name('category.create')
+Route::post('/kategori', [KategoriController::class, 'store']);
 
 // Routing UserController
 Route::get('/user', [UserController::class, 'index']);
