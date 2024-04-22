9644323150af37a6285528dc05dcd712913b59b0
Ariel Saputra <m15759246@gmail.com>
2024-04-22 22:49:00 +0700

    Jobsheet9 - Auth dan Middleware

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,6 +15,6 @@ class WelcomeController extends Controller
 
         $activeMenu = 'dashboard';
 
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        return view('login', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,25 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = "m_user";
-    public $timestamps = false;
-    protected $primaryKey = 'user_id';
-    protected $fillable = [
-        'user_id',
-        'level_id',
-        'username',
-        'nama',
-        'password',
-    ];
+    protected $table = 'm_user';
+    protected $primaryKey = "user_id";
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level()
+    public function level(): BelongsTo
     {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id');
     }
+
+    public function userLainnya(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'user_id');
+    }
+
+    // public function stok(): HasMany
+    // {
+    //     return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    // }
+
+    // public function penjualan(): HasMany
+    // {
+    //     return $this->hasMany(TransaksiModel::class, 'user_id', 'user_id');
+    // }
 }
