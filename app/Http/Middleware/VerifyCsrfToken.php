f7a2dc0b2936655f9128b244b202f8e78ff40d10
Ariel Saputra <m15759246@gmail.com>
2024-04-25 23:27:31 +0700

    Jobsheet10 - Praktikum1-Membuat RESTful API Register

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/kategori'
+        '/kategori',
+        '/register',
     ];
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,36 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
-
+    public function getJWTIdentifier()
+    {
+        return $this->getkey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+    ];
     protected $table = 'm_user';
-    protected $primaryKey = "user_id";
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id');
-    }
-
-    public function userLainnya(): HasMany
-    {
-        return $this->hasMany(UserModel::class, 'user_id');
-    }
-
-    // public function stok(): HasMany
-    // {
-    //     return $this->hasMany(StokModel::class, 'user_id', 'user_id');
-    // }
-
-    // public function penjualan(): HasMany
-    // {
-    //     return $this->hasMany(TransaksiModel::class, 'user_id', 'user_id');
-    // }
+    protected $primaryKey = 'user_id';
 }
