ce6646cfdb20f2c18ec14f6afa5eb2aa58c533ff
Ariel Saputra <m15759246@gmail.com>
2024-04-29 20:42:44 +0700

    Jobsheet10 - Tugas Done Fix

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,28 +2,45 @@
 
 namespace App\Models;
 
+use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Database\Eloquent\Model;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+
+    use Notifiable;
+
     public function getJWTIdentifier()
     {
-        return $this->getkey();
+        return $this->getKey();
     }
+
     public function getJWTCustomClaims()
     {
         return [];
     }
+
+    use HasFactory;
+
+    protected $table = "m_user";
+    public $timestamps = false;
+    protected $primaryKey = 'user_id';
     protected $fillable = [
+        'user_id',
+        'level_id',
         'username',
         'nama',
         'password',
-        'level_id',
     ];
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
